c1be409f2c228b661ae2795f90744cd541051bbf
92Infinitus92 <92georgepetroff92@gmail.com>
2024-11-07 14:11:28 +0200

    procuding with the schema of the store/studio

---
+++ sanity/schemaTypes/orderType.ts
@@ -0,0 +1,92 @@
+import { BasketIcon } from "@sanity/icons";
+import { title } from "process";
+import { defineArrayMember, defineField, defineType, validation } from "sanity";
+
+export const orderType = defineType({
+  name: "order",
+  title: "Order",
+  type: "document",
+  icon: BasketIcon,
+  fields: [
+    defineField({
+      name: "orderNumber",
+      title: "Order Number",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "stripeCheckoutSessionId",
+      title: "Stripe Checkout Session ID",
+      type: "string",
+    }),
+    defineField({
+      name: "stripeCustomerId",
+      title: "Stripe Customer ID",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "clerkUserId",
+      title: "Store User ID",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "customerName",
+      title: "Customer Name",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "customerEmail",
+      title: "Customer Email",
+      type: "string",
+      validation: (Rule) => Rule.required().email(),
+    }),
+    defineField({
+      name: "stripePaymentIntentId",
+      title: "Stripe Payment Intent ID",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "products",
+      title: "Products Bought",
+      type: "array",
+      of: [
+        defineArrayMember({
+          type: "object",
+          fields: [
+            defineField({
+              name: "product",
+              title: "Product Bought",
+              type: "reference",
+              to: [{ type: "product" }],
+            }),
+            defineField({
+              name: "quantity",
+              title: "Quantity Purchased",
+              type: "number",
+            }),
+          ],
+          preview: {
+            select: {
+              product: "product.name",
+              quantity: "quantity",
+              image: "product.image",
+              price: "product.price",
+              currency: "product.currency",
+            },
+            prepare(select) {
+              return {
+                title: `${select.quantity} x ${select.product} - ${select.price} ${select.currency}`,
+                media: select.image,
+                subtitle: `${select.quantity} x ${select.price} ${select.currency}`,
+              };
+            },
+          },
+        }),
+      ],
+    }),
+  ],
+});
